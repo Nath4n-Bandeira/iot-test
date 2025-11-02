2f0742f01bb502f7165584285bd77fa31b748983
Nathan Bandeira Pedrozo <143651576+Nath4n-Bandeira@users.noreply.github.com>
2025-11-02 16:23:23 -0300

    Add hydration state to ClienteContext

--- front/app/context/ClienteContext.ts
+++ front/app/context/ClienteContext.ts
@@ -1,23 +1,31 @@
-// context/ClienteContext.ts
-import { ClienteItf } from '../utils/types/ClienteItf'
-import { create } from 'zustand'
-import { persist } from 'zustand/middleware'
+import type { ClienteItf } from "../utils/types/ClienteItf"
+import { create } from "zustand"
+import { persist } from "zustand/middleware"
 
 type ClienteStore = {
-    cliente: ClienteItf
-    logaCliente: (clienteLogado: ClienteItf) => void
-    deslogaCliente: () => void
+  cliente: ClienteItf
+  logaCliente: (clienteLogado: ClienteItf) => void
+  deslogaCliente: () => void
+  isHydrated: boolean
+  setIsHydrated: (hydrated: boolean) => void
 }
 
 export const useClienteStore = create<ClienteStore>()(
-    persist(
-        (set) => ({
-            cliente: {} as ClienteItf,
-            logaCliente: (clienteLogado) => set({ cliente: clienteLogado }),
-            deslogaCliente: () => set({ cliente: {} as ClienteItf })
-        }),
-        {
-            name: 'cliente-storage', 
+  persist(
+    (set) => ({
+      cliente: {} as ClienteItf,
+      isHydrated: false,
+      logaCliente: (clienteLogado) => set({ cliente: clienteLogado }),
+      deslogaCliente: () => set({ cliente: {} as ClienteItf }),
+      setIsHydrated: (hydrated) => set({ isHydrated: hydrated }),
+    }),
+    {
+      name: "cliente-storage",
+      onRehydrateStorage: () => (state) => {
+        if (state) {
+          state.setIsHydrated(true)
         }
-    )
+      },
+    },
+  ),
 )
